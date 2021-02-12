a48cbe01ad238e75098e5806b84b3e6f43facbf6
lokbiswa <71455137+lokbiswa@users.noreply.github.com>
2021-02-12 14:05:43 -0600

    connected MongoDB
    created variables in .env
    -  connection strings
    -  port number
    - added sample data on mongoDB

--- server.js
+++ server.js
@@ -1,2 +1,34 @@
 const express = require('express');
-const BodyParser = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config(); 
+// getting connection string from .env
+connectionString = process.env.CONNECTION_STRING;
+// port # from .env
+port = process.env.PORT;
+
+// connection to mongoDB
+MongoClient.connect(connectionString,{useUnifiedTopology: true}) .then(client => {
+
+  const db = client.db('capstone');
+  const quotesCollections = db.collection('ticket');
+  console.log('connected to database');
+  
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+//   Add all the CRUD here!
+
+  // Get Method
+  app.get('/', (req, res) =>{
+      data = db.collection('tickets').find().toArray();
+      data.then(result => res.send(result));
+  })
+
+
+  // localhost
+  app.listen(port, function() {
+    console.log(`listening on: http://localhost:${port}`)
+  })
+})
+.catch(error => console.error(error))
