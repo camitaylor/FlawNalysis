e4e7392f0f11cdc7219858a48c51ee4a298101b0
Cami Taylor <73480669+camitaylor@users.noreply.github.com>
2021-02-17 16:26:28 -0600

    Update server.js

--- models/server.js
+++ models/server.js
@@ -12,7 +12,7 @@ connectionString = `mongodb+srv://${username}:${password}@cluster0.d0ygw.mongodb
 port = process.env.PORT;
 
 //add model Ticket
-const Ticket = require('./ticket')
+const Ticket = require('./models/ticket')
 
 
 // connection to mongoDB
