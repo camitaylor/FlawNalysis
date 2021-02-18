9cab602d24a825a6285d297628cedada115213ab
lokbiswa <71455137+lokbiswa@users.noreply.github.com>
2021-02-18 13:15:18 -0600

    formated Main.js

--- public/main.js
+++ public/main.js
@@ -5,6 +5,13 @@ fetch('/tickets').then(res => {
   })
   .then(respose => {
       respose.forEach(data => {
-        document.getElementById('tableBody').innerHTML += `<tr><td class = "name" >${data.name}</td><td class = "details">${data.ticketDetails}</td><td class = "Assigned">${data.assignedTo}</td><td class = "date">${data.requestedDate}</td><td class = "editButton"><a href = "#" value = ${data._id}><i class="glyphicon glyphicon-edit"></i></a></td><td class = "editButton"><a href = "#" value = ${data._id}><i class="glyphicon glyphicon-remove"></i></a></td></tr>`
+        document.getElementById('tableBody').innerHTML += `<tr>
+        <td class = "name" >${data.name}</td>
+        <td class = "details">${data.ticketDetails}</td>
+        <td class = "Assigned">${data.assignedTo}</td>
+        <td class = "date">${data.requestedDate}</td>
+        <td class = "editButton"><a href = "#" value = ${data._id}><i class="glyphicon glyphicon-edit"></i></a></td>
+        <td class = "editButton"><a href = "#" value = ${data._id}><i class="glyphicon glyphicon-remove"></i></a></td>
+        </tr>`
       });
   })
