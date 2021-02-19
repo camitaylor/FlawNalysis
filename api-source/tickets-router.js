89b3ba62e1be294f15138f4a0811ce708fc80b71
lokbiswa <71455137+lokbiswa@users.noreply.github.com>
2021-02-19 11:38:17 -0600

    updated

--- api-source/tickets-router.js
+++ api-source/tickets-router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient;
 const { Router } = require('express');
+// const ticket = require('../models/ticket');
 require('dotenv').config();
 // getting credential to connect to db
 username = process.env.USER
@@ -30,11 +31,9 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(client 
     data.then(result => res.send(result))
       .catch(error => console.error(error));
   })
-  router.get('/:id', (req, res) => {
-    data = db.collection('tickets').findOne(
-      { 
-        id: req.body.id 
-      });
+  router.get('/:name', (req, res) => {
+    console.log(req.params.name)
+    data = db.collection('tickets').findOne({id: req.params.name});
     data.then(result => res.send(result))
       .catch(error => console.error(error));
   })
@@ -69,26 +68,27 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(client 
 
   //Delete Method
   router.delete('/:id', (req, res) => {
-    data = db.collection('tickets').deleteOne(
-      {
-        id: req.body.id,
-        name: req.body.name,
-        type: req.body.type,
-        requestedDate: req.body.requestedDate,
-        assignedTo: req.body.assignedTo,
-        ticketDetails: req.body.ticketDetails
-      }
-    )
-    data.then(result => {
-      if (result.deletedCount === 0) {
-        return res.json('No ticket to delete')
-      }
-      res.json(`Ticket deleted`)
+    console.log(req.body)
+    db.collection('tickets').deleteOne({
+      id: req.body.id,
+      name: req.body.name,
+      type: req.body.type,
+      requestedDate: req.body.requestedDate,
+      assignedTo: req.body.assignedTo,
+      ticketDetails: req.body.ticketDetails
     })
+    .then(result => {
+        if (result.deletedCount === 0) {
+          return res.json('No quote to delete')
+        }
+        res.json(`Deleted Darth Vadar's quote`)
+      })
       .catch(error => console.error(error))
   })
-
 })
+<<<<<<< Updated upstream
   .catch(error => console.error(error))
 
+=======
+>>>>>>> Stashed changes
 module.exports = router
--- public/main.js
+++ public/main.js
@@ -11,7 +11,76 @@ fetch('/tickets').then(res => {
         <td class = "Assigned">${data.assignedTo}</td>
         <td class = "date">${data.requestedDate}</td>
         <td class = "editButton"><a href = "#" value = ${data._id}><i class="glyphicon glyphicon-edit"></i></a></td>
-        <td class = "editButton"><a href = "#" value = ${data._id}><i class="glyphicon glyphicon-remove"></i></a></td>
+        <td class = "editButton"><button class = "remove" href = "#" value = ${data._id}><i class="glyphicon glyphicon-remove"></i></button></td>
         </tr>`
       });
   })
+
+if (document.addEventListener) {
+  document.addEventListener("click", findClickedRowRemove, false);
+}
+else if (document.attachEvent) {
+  document.attachEvent("onclick", findClickedRowRemove);
+}
+
+function findClickedRowRemove(event) {
+  event = event || window.event;
+  event.target = event.target || event.srcElement;
+
+  var element = event.target;
+
+  // Climb up the document tree from the target of the event
+  while (element) {
+      if (element.nodeName === "BUTTON" && /remove/.test(element.className)) {
+        deleteTicket(element)
+        break;
+      }
+
+      element = element.parentNode;
+  }
+}
+
+
+function deleteTicket(element){
+  _id = element.value;
+  console.log(_id)
+  url = `/tickets/ObjectId(${_id})`;
+  fetch(url).then(res => {
+    if(res.body.id){
+        return res.json();
+    }
+  })
+  .then(result =>{
+    console.log(result)
+  //   fetch('/tickets',{
+  //     method: 'delete',
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify(result)
+  //   }).then(res=> res.json)
+  // .then(result => console.log(result))
+  })
+}
+// if (document.addEventListener) {
+//   document.addEventListener("click", findClickedRow, false);
+// }
+// else if (document.attachEvent) {
+//   document.attachEvent("onclick", findClickedRow);
+// }
+
+// function findClickedRow(event) {
+//   event = event || window.event;
+//   event.target = event.target || event.srcElement;
+
+//   var element = event.target;
+
+//   // Climb up the document tree from the target of the event
+//   while (element) {
+//       if (element.nodeName === "BUTTON" && /remove/.test(element.className)) {
+//         console.log()
+//         break;
+//       }
+
+//       element = element.parentNode;
+//   }
+// }
+
