85ea303f67cc02cb11985f3fa220fd61dd5109ff
lokbiswa <71455137+lokbiswa@users.noreply.github.com>
2021-02-24 14:18:49 -0600

    updated

--- public/main.js
+++ public/main.js
@@ -1,25 +1,21 @@
 fetch('/tickets').then(res => {
-  if (res.ok) {
-    return res.json();
-  }
-})
+    if(res.ok){
+        return res.json();
+    }
+  })
   .then(respose => {
       respose.forEach(data => {
         document.getElementById('tableBody').innerHTML += `<tr>
-        <td class = "col-2" >${data.name}</td>
-        <td class = "col-3">${data.ticketDetails}</td>
-        <td class = "col-2">${data.assignedTo}</td>
-        <td class = "col-1">${data.requestedDate}</td>
-        <td class = "col-1">${data.status}</td>
-        <td class = "${data.priority} col-1">${data.priority}</td>
-        <td class = "editButton col-1"><button class ="edit" title = "click to Edit" value = ${data._id}><i class="glyphicon glyphicon-pencil"></i> Edit</buton></td>
-        <td class = "editButton col-1"><button class = "remove" title = "click to delete" value = ${data._id}><i class="glyphicon glyphicon-trash"></i> Delete</button></td>
+        <td class = "name" >${data.name}</td>
+        <td class = "details">${data.ticketDetails}</td>
+        <td class = "Assigned">${data.assignedTo}</td>
+        <td class = "date">${data.requestedDate}</td>
+        <td class = "editButton"><a href = "#" value = ${data._id}><i class="glyphicon glyphicon-edit"></i></a></td>
+        <td class = "editButton"><button class = "remove" href = "#" value = ${data._id}><i class="glyphicon glyphicon-remove"></i></button></td>
         </tr>`
-    });
+      });
   })
 
-
-// listening to button clicked
 if (document.addEventListener) {
   document.addEventListener("click", findClickedRowRemove, false);
 }
@@ -27,49 +23,64 @@ else if (document.attachEvent) {
   document.attachEvent("onclick", findClickedRowRemove);
 }
 
-// TO identify the row where delete and edit buttons are clicked.
 function findClickedRowRemove(event) {
   event = event || window.event;
   event.target = event.target || event.srcElement;
 
-  let element = event.target;
+  var element = event.target;
 
   // Climb up the document tree from the target of the event
   while (element) {
-    // finds clicked Delete Button
-    if (element.nodeName === "BUTTON" && /remove/.test(element.className)) {
-      console.log(element.value)
-      sessionStorage.setItem("id", `${element.value}`);
-      window.location.href = '#popup1'
-      break;
-    }
-    // finds clicked update button. 
-    else if (element.nodeName === "BUTTON" && /edit/.test(element.className)) {
-      console.log(element.value)
-      sessionStorage.setItem("id", `${element.value}`);
-      window.location.href = '/updateTicket.html'
-      break;
-    }
-    element = element.parentNode;
+      if (element.nodeName === "BUTTON" && /remove/.test(element.className)) {
+        deleteTicket(element)
+        break;
+      }
+
+      element = element.parentNode;
   }
 }
 
 
-function deleteTicket() {
-  id = sessionStorage.getItem('id');
-  url = `/tickets/${id}`;
-  fetch(url, {
-    method: 'delete'
-  }).then(res => {
-    return res.json();
+function deleteTicket(element){
+  _id = element.value;
+  console.log(_id)
+  url = `/tickets/ObjectId(${_id})`;
+  fetch(url).then(res => {
+    if(res.body.id){
+        return res.json();
+    }
   })
   .then(result =>{
     console.log(result)
-    sessionStorage.removeItem('id')
-    window.location.href='http://localhost:3030/dashboard.html';
-})
-
+  //   fetch('/tickets',{
+  //     method: 'delete',
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify(result)
+  //   }).then(res=> res.json)
+  // .then(result => console.log(result))
+  })
 }
+// if (document.addEventListener) {
+//   document.addEventListener("click", findClickedRow, false);
+// }
+// else if (document.attachEvent) {
+//   document.attachEvent("onclick", findClickedRow);
+// }
 
+// function findClickedRow(event) {
+//   event = event || window.event;
+//   event.target = event.target || event.srcElement;
 
+//   var element = event.target;
+
+//   // Climb up the document tree from the target of the event
+//   while (element) {
+//       if (element.nodeName === "BUTTON" && /remove/.test(element.className)) {
+//         console.log()
+//         break;
+//       }
+
+//       element = element.parentNode;
+//   }
+// }
 
