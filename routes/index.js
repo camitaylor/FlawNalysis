04bdf240450c3d937154c1c39da6497fed9ec27f
ohampton <44102431+ohampton@users.noreply.github.com>
2021-02-24 11:22:38 -0500

    commit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   // res.render('index', { title: 'Auth0 Webapp sample Nodejs' });
-  res.send('There is no war in ba sing se')
+  res.redirect('/index.html')
 });
 
 module.exports = router;
